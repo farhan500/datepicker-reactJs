4a2777a9888dc6065a9e1c9bec67889f13d6cb72
farhan <sarmadfarhankhan@gmail.com>
2019-09-18 20:14:07 +0530

    'datePicker'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DatePicker from './components/datePicker';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <DatePicker/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/datePicker.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css'
+
+import M from 'materialize-css/dist/js/materialize.min.js'
+
+
+function DatePicker() {
+    document.addEventListener('DOMContentLoaded', function() {     
+
+        var elems = document.querySelectorAll('.datepicker');        
+        const options = { format: 'dd/mm/yyyy', };
+        var instances = M.Datepicker.init(elems, options);
+      
+      });
+    return (
+      <div className="DP" style={{width:100}}>
+   <input type="text" className="datepicker">{}</input>
+      </div>
+    );
+  }
+  
+  export default DatePicker;
