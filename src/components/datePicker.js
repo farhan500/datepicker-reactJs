90e782c8ea1668dbabf794d45b4f5998db34801d
farhan <sarmadfarhankhan@gmail.com>
2019-09-18 22:04:05 +0530

    update

--- src/components/datePicker.js
+++ src/components/datePicker.js
@@ -3,23 +3,21 @@ import 'materialize-css/dist/css/materialize.min.css'
 
 import M from 'materialize-css/dist/js/materialize.min.js'
 
-
-function DatePicker() {
-    let datepickerRef = null;
-
-    document.addEventListener('DOMContentLoaded', function() {  
-     
-        var elems = datepickerRef;        
-        const options = { format: 'dd/mm/yyyy', };
-        M.Datepicker.init(elems, options);
-      
-      });
-    return (
-      <div className="DP" style={{width:100}}>
-   <input type="text" className="datepicker" ref={(holder) => datepickerRef = holder} ></input>
-
-      </div>
-    );
-  }
-  
-  export default DatePicker;
+export default class DatePicker extends React.Component {
+    constructor() {
+        super();
+        this.datepickerRef = null;
+    }
+    componentDidMount() {
+        const options = { format: 'dd/mm/yyyy' };
+        M.Datepicker.init(this.datepickerRef, options);
+    }
+    render() {
+        return (
+            <div className="DP" style={{ width: 100 }}>
+                <input type="text" className="datepicker" ref={holder => this.datepickerRef = holder}></input>
+            </div>
+        );
+    }
+}
+ 
